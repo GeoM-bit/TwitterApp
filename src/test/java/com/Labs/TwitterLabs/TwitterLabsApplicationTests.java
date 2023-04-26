e692cd690033618c0b5c2f8a16b9de9ed9806874
Georgiana Matei <matei.georgiana11@gmail.com>
2023-04-26 17:05:17 +0300

    Initial project structure

---
+++ src/test/java/com/Labs/TwitterLabs/TwitterLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Labs.TwitterLabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
